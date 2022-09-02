675537c2dc5d26eb52d4672a97406127d9b7ebb7
Thomas Epperson <thomas.epperson@gmail.com>
2022-09-02 12:31:17 +0000

    Delete target folder from example

--- example/target/debug/deps/multiwin_demo-71ac50cf20aeda18.d
+++
@@ -1,5 +0,0 @@
-c:\Users\2311thep\Documents\egui-glow-multiwin\src\example\target\debug\deps\multiwin_demo-71ac50cf20aeda18.rmeta: src\main.rs
-
-c:\Users\2311thep\Documents\egui-glow-multiwin\src\example\target\debug\deps\multiwin_demo-71ac50cf20aeda18.d: src\main.rs
-
-src\main.rs:
--- example/target/debug/deps/multiwin_demo-8a027c28b26587f6.d
+++
@@ -1,5 +0,0 @@
-c:\Users\2311thep\Documents\egui-glow-multiwin\src\example\target\debug\deps\multiwin_demo-8a027c28b26587f6.rmeta: src\main.rs
-
-c:\Users\2311thep\Documents\egui-glow-multiwin\src\example\target\debug\deps\multiwin_demo-8a027c28b26587f6.d: src\main.rs
-
-src\main.rs:
